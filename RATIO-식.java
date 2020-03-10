114e0b3c4aa754e03f219545798169cf1fd506d1
pjy1368 <56083021+pjy1368@users.noreply.github.com>
2020-03-11 00:25:37 +0900

    Create RATIO-식.java

---
+++ RATIO-식.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st;
+
+		int T = Integer.parseInt(br.readLine());
+		final int MAX = 2000000000;
+		for (int i = 0; i < T; i++) {
+			st = new StringTokenizer(br.readLine());
+			long N = Long.parseLong(st.nextToken()); // 플레이 횟수
+			long M = Long.parseLong(st.nextToken()); // 승리 횟수
+			long Z = (M * 100) / N; // 승률
+
+			if (Z >= 99) {
+				bw.write("-1\n");
+				continue;
+			}
+
+			Z++;
+			int x = (int) Math.ceil((N * Z - 100 * M) / (double) (100 - Z));
+			bw.write(x + "\n");
+		}
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+
+}
